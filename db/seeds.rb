187077d67ab1a726ceecf69809711b446741abed
vascocorreia <vascocorreia99@gmail.com>
2022-11-16 12:56:25 +0000

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+5.times do
+    ["chinese", "italian", "japanese", "french", "belgian"]
+    Restaurant.create(
+      name: Faker::Book.title,
+      address: Faker::Address.street_name,
+      phone_number: Faker::Number.number(digits: 10),
+      category: ["chinese", "italian", "japanese", "french", "belgian"].shuffle.first
+    ) 
+end
